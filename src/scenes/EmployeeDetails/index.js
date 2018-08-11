36cde010e26157d1dadd069b30305105ade17dad
marcinludwig <marcinl@eb2b.com.pl>
2018-08-11 08:35:41 +0200

    Fix error with broken navigation

--- src/scenes/EmployeeDetails/index.js
+++ src/scenes/EmployeeDetails/index.js
@@ -3,23 +3,19 @@ import employees from './../../store/employee'
 import './EmployeeDetails.css'
 
 class EmployeeDetails extends PureComponent {
-  state = {
-    employee: employees.find(employee => employee.id === parseInt(this.props.match.params.id) )
-  }
-
   minId = employees.reduce((prev, curr) => prev.id < curr.id ? prev : curr).id
 
   maxId = employees.reduce((prev, curr) => prev.id > curr.id ? prev : curr).id
 
   handlePrevButtonClick = () => {
-    let prevId = this.state.employee.id - 1
-    if (prevId < this.minId) prevId = this.minId
+    let prevId = parseInt(this.props.match.params.id) - 1
+    if (prevId < this.minId) prevId = this.maxId
     this.goToMember(prevId)
   }
 
   handleNextButtonClick = () => {
-    let nextId = this.state.employee.id + 1
-    if (nextId > this.maxId) nextId = this.maxId
+    let nextId = parseInt(this.props.match.params.id) + 1
+    if (nextId > this.maxId) nextId = this.minId
     this.goToMember(nextId)
   }
 
@@ -30,15 +26,16 @@ class EmployeeDetails extends PureComponent {
   goToMember = memberId => this.props.history.push(`/member/${memberId}`)
 
   render() {
-    console.log('state', this.minId, this.maxId)
-    if (!this.state.employee) return
+    const employee = employees.find(employee => employee.id === parseInt(this.props.match.params.id))
+
+    if (!employee) return
     return (
       <div className="employee-details">
-        <div className="wrapper">
+        <div className="employee-details-wrapper">
           <div className="prev" onClick={this.handlePrevButtonClick} />
           <div className="info">
-            <div className="role">{this.state.employee.role}</div>
-            <div className="name">{this.state.employee.name}</div>
+            <div className="role">{employee.role}</div>
+            <div className="name">{employee.name}</div>
           </div>
           <div className="next" onClick={this.handleNextButtonClick} />
           <div className="close" onClick={this.handleCloseButtonClick} />
